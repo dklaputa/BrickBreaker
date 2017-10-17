4bbc5f2d5f942c8e13a18cef097e25a1dfa97680
dklaputa <d.k.laputa@gmail.com>
2017-10-17 23:03:14 +0900

    Add fps.

---
+++ Assets/Scripts/FpsScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsScript : MonoBehaviour
+{
+	Text text;
+	// Use this for initialization
+	void Start ()
+	{
+		text = GameObject.Find ("FPS").GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		text.text = (1f / Time.deltaTime).ToString();
+	}
+}
