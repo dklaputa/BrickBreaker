c8b12feeaaef705cec88f7f5f5fb3014a3ec29b9
dklaputa <d.k.laputa@gmail.com>
2018-01-08 19:43:13 +0900

    Resolution adaption.

--- Assets/Scripts/StoreController.cs
+++ Assets/Scripts/StoreController.cs
@@ -4,9 +4,12 @@ using UnityEngine.UI;
 
 public class StoreController : MonoBehaviour
 {
+    private readonly Color starNotEnough = new Color(255f / 255, 135f / 255, 135f / 255);
     private Text totalStar;
     private Text divisionCount;
     private Text blackHoleCount;
+    private Text divisionCost;
+    private Text blackHoleCost;
 
     public void onExitClick()
     {
@@ -18,36 +21,48 @@ public class StoreController : MonoBehaviour
         if (!PlayerPrefs.HasKey("DivisionCount")) PlayerPrefs.SetInt("DivisionCount", 1);
         if (!PlayerPrefs.HasKey("BlackHoleCount")) PlayerPrefs.SetInt("BlackHoleCount", 1);
         totalStar = GameObject.Find("TotalStarNum").GetComponent<Text>();
-        totalStar.text = PlayerPrefs.GetInt("StarNumber").ToString();
+        var star = PlayerPrefs.GetInt("StarNumber");
+        totalStar.text = star.ToString();
         divisionCount = GameObject.Find("DivisionCount").GetComponent<Text>();
         divisionCount.text = PlayerPrefs.GetInt("DivisionCount").ToString();
         blackHoleCount = GameObject.Find("BlackHoleCount").GetComponent<Text>();
         blackHoleCount.text = PlayerPrefs.GetInt("BlackHoleCount").ToString();
+        divisionCost = GameObject.Find("DivisionCost").GetComponent<Text>();
+        blackHoleCost = GameObject.Find("BlackHoleCost").GetComponent<Text>();
+        if (star >= 10) return;
+        divisionCost.color = starNotEnough;
+        blackHoleCost.color = starNotEnough;
     }
 
     public void onBuyDivisionItem()
     {
         var star = PlayerPrefs.GetInt("StarNumber");
-        if (star < 50) return;
+        if (star < 10) return;
         var item = PlayerPrefs.GetInt("DivisionCount");
-        star -= 50;
+        star -= 10;
         item++;
         PlayerPrefs.SetInt("StarNumber", star);
         PlayerPrefs.SetInt("DivisionCount", item);
         totalStar.text = star.ToString();
         divisionCount.text = item.ToString();
+        if (star >= 10) return;
+        divisionCost.color = starNotEnough;
+        blackHoleCost.color = starNotEnough;
     }
 
     public void onBuyBlackHoleItem()
     {
         var star = PlayerPrefs.GetInt("StarNumber");
-        if (star < 50) return;
+        if (star < 10) return;
         var item = PlayerPrefs.GetInt("BlackHoleCount");
-        star -= 50;
+        star -= 10;
         item++;
         PlayerPrefs.SetInt("StarNumber", star);
         PlayerPrefs.SetInt("BlackHoleCount", item);
         totalStar.text = star.ToString();
         blackHoleCount.text = item.ToString();
+        if (star >= 10) return;
+        divisionCost.color = starNotEnough;
+        blackHoleCost.color = starNotEnough;
     }
 }
